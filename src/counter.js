aaca39e4adc3204ae0808af0ec19463b73633f75
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-01-12 19:25:54 +0530

    button disabled added

--- src/counter.js
+++ src/counter.js
@@ -1,30 +1,36 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 
+export default function Counter() {
+  const [count, setCount] = useState(0);
 
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+  function handleDecrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+  function handleReset() {
+    setCount(0);
+  }
 
-export default function Counter(){
-    const [count,setCount] = useState(0);
-    
-    function handleIncrement(){
-        setCount(count+1)
-    }
-    function handleDecrement(){
-        setCount(count-1)
-    }
-    function handleReset(){
-        setCount(0)
-    }
-   
-    return(
-        <div className="container">
-            <div>
-                <p className="count">{count}</p>
-            </div>
-            <div className="button">
-            <button onClick = {handleIncrement} className="btn">Increment</button>
-            <button onClick = {handleDecrement} className="btn">Decrement</button>
-            <button onClick = {handleReset} className="btn">Reset</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div>
+        <p className="count">{count}</p>
+      </div>
+      <div className="button">
+        <button onClick={handleIncrement} className="btn">
+          Increment
+        </button>
+        <button onClick={handleDecrement} className="btn ">
+          Decrement
+        </button>
+        <button onClick={handleReset} className="btn" disabled = {count === 0}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
